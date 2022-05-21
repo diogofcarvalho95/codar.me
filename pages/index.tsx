67f29678a23c1fd7418cb76c6c5700f8b06c4cd0
Diogo Carvalho <diogofcarvalho95@gmail.com>
2022-05-22 00:41:38 +0100

    classname fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,62 +25,62 @@ const Home: NextPage = () => {
             <Contact/>
           </section>
         </main>
-        <div class="text-gray-300 hidden md:flex md:gap-1 md:items-center md:cursor-pointer md:pb-8">
-          <svg viewBox="0 0 25 25" class="h-3" fill="currentColor" width="25"><path d="M13.5 2a1.5 1.5 0 00-3 0h3zm-2.56 22.06a1.5 1.5 0 002.12 0l9.547-9.545a1.5 1.5 0 10-2.122-2.122L12 20.88l-8.485-8.486a1.5 1.5 0 10-2.122 2.122l9.546 9.546zM10.5 2v21h3V2h-3z"></path></svg>
+        <div className="text-gray-300 hidden md:flex md:gap-1 md:items-center md:cursor-pointer md:pb-8">
+          <svg viewBox="0 0 25 25" className="h-3" fill="currentColor" width="25"><path d="M13.5 2a1.5 1.5 0 00-3 0h3zm-2.56 22.06a1.5 1.5 0 002.12 0l9.547-9.545a1.5 1.5 0 10-2.122-2.122L12 20.88l-8.485-8.486a1.5 1.5 0 10-2.122 2.122l9.546 9.546zM10.5 2v21h3V2h-3z"></path></svg>
           <a href="#project">Saiba mais</a>
         </div>
       </div>
-      <section id="project" class="container mx-auto md:max-w-6xl pt-12 lg:py-20">
-        <h2 class="mb-0 text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">O que você vai aprender</h2>
+      <section id="project" className="container mx-auto md:max-w-6xl pt-12 lg:py-20">
+        <h2 className="mb-0 text-3xl md:text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">O que você vai aprender</h2>
         <p className="text-gray-300 text-base mt-3 md:mt-4 lg:mt-5 max-w-2xl leading-relaxed">Nós vamos <strong className="text-white">criar um mini clone do Twitter</strong>, iniciando com HTML e CSS apenas, evoluindo para frameworks como React e Tailwind. Sempre falando de como tudo funciona, para que <strong className="text-white">mesmo que não entende nada de programação</strong> possa entender e acompanhar.</p>
-        <div class="mt-6 md:mt-8 lg:mt-10 flex flex-wrap gap-6">
-          <div class="max-w-lg min-w-[40%] lg:min-w-[1%] flex-1">
-            <img class="rounded aspect-video lg:max-w-[164px]" src="/aula-01.png"/>
-            <div class="mt-6 text-3xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">30.05</div>
-            <span class="text-gray-300">O começo de tudo</span>
+        <div className="mt-6 md:mt-8 lg:mt-10 flex flex-wrap gap-6">
+          <div className="max-w-lg min-w-[40%] lg:min-w-[1%] flex-1">
+            <img className="rounded aspect-video lg:max-w-[164px]" src="/aula-01.png"/>
+            <div className="mt-6 text-3xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">30.05</div>
+            <span className="text-gray-300">O começo de tudo</span>
           </div>
-          <div class="max-w-lg min-w-[40%] lg:min-w-[1%] flex-1">
-            <img class="rounded aspect-video lg:max-w-[164px]" src="/aula-02.png"/>
-            <div class="mt-6 text-3xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">31.05</div>
-            <span class="text-gray-300">Algumas abstrações</span>
+          <div className="max-w-lg min-w-[40%] lg:min-w-[1%] flex-1">
+            <img className="rounded aspect-video lg:max-w-[164px]" src="/aula-02.png"/>
+            <div className="mt-6 text-3xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">31.05</div>
+            <span className="text-gray-300">Algumas abstrações</span>
           </div>
-          <div class="max-w-lg min-w-[40%] lg:min-w-[1%] flex-1">
-            <img class="rounded aspect-video lg:max-w-[164px]" src="/aula-03.png"/>
-            <div class="mt-6 text-3xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">01.06</div>
-            <span class="text-gray-300">O coração do projeto</span>
+          <div className="max-w-lg min-w-[40%] lg:min-w-[1%] flex-1">
+            <img className="rounded aspect-video lg:max-w-[164px]" src="/aula-03.png"/>
+            <div className="mt-6 text-3xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">01.06</div>
+            <span className="text-gray-300">O coração do projeto</span>
           </div>
-          <div class="max-w-lg min-w-[40%] lg:min-w-[1%] flex-1">
-            <img class="rounded aspect-video lg:max-w-[164px]" src="/aula-04.png"/>
-            <div class="mt-6 text-3xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">02.06</div>
-            <span class="text-gray-300">Tudo funcionando</span>
+          <div className="max-w-lg min-w-[40%] lg:min-w-[1%] flex-1">
+            <img className="rounded aspect-video lg:max-w-[164px]" src="/aula-04.png"/>
+            <div className="mt-6 text-3xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">02.06</div>
+            <span className="text-gray-300">Tudo funcionando</span>
           </div>
-          <div class="max-w-lg min-w-[40%] lg:min-w-[1%] flex-1">
-            <img class="rounded aspect-video lg:max-w-[164px]" src="/aula-05.png"/>
-            <div class="mt-6 text-3xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">03.06</div>
-            <span class="text-gray-300">Mostre ao mundo!</span>
+          <div className="max-w-lg min-w-[40%] lg:min-w-[1%] flex-1">
+            <img className="rounded aspect-video lg:max-w-[164px]" src="/aula-05.png"/>
+            <div className="mt-6 text-3xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">03.06</div>
+            <span className="text-gray-300">Mostre ao mundo!</span>
           </div>
         </div>
       </section>
-      <section id="about" class="container mx-auto md:max-w-6xl pt-12 pb-12 lg:py-20">
-        <div class="md:flex md:gap-5 md:items-center">
+      <section id="about" className="container mx-auto md:max-w-6xl pt-12 pb-12 lg:py-20">
+        <div className="md:flex md:gap-5 md:items-center">
           <div>
-            <h2 class="text-3xl lg:text-4xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">Conheça seu professor</h2>
-            <p class="text-gray-300 text-base mt-3 md:mt-4 lg:mt-5 max-w-2xl leading-relaxed">Qualquer pessoa tem algo a ensinar. Mas <strong className="text-white">só quem conhece o mercado e tem muita experiência em projetos reais</strong>, sabe exatamente o caminho para se tornar um profissional acima da média.</p>
+            <h2 className="text-3xl lg:text-4xl font-black text-transparent bg-clip-text bg-gradient-to-br from-gradientSky-0 to-gradientSky-1">Conheça seu professor</h2>
+            <p className="text-gray-300 text-base mt-3 md:mt-4 lg:mt-5 max-w-2xl leading-relaxed">Qualquer pessoa tem algo a ensinar. Mas <strong className="text-white">só quem conhece o mercado e tem muita experiência em projetos reais</strong>, sabe exatamente o caminho para se tornar um profissional acima da média.</p>
           </div>
-          <div class="bg-gray-800 p-3 gap-4 rounded-3xl flex flex-col md:flex-row items-center mt-8 md:mt-0">
-            <div class="pl-5">
+          <div className="bg-gray-800 p-3 gap-4 rounded-3xl flex flex-col md:flex-row items-center mt-8 md:mt-0">
+            <div className="pl-5">
               <Image src="/avatar_blue.png" alt="Bruno Bertolini" width={190} height={240}/>
             </div>
-            <div class="py-3 pr-5 flex-1">
-              <div class="py-3">
-                <span class="uppercase text-mute text-gray-300 text-sm">+ 16 anos de experiência</span>
-                <h2 class="text-2xl md:text-3xl font-bold text-white mt-2">Bruno Bertolini</h2>
+            <div className="py-3 pr-5 flex-1">
+              <div className="py-3">
+                <span className="uppercase text-mute text-gray-300 text-sm">+ 16 anos de experiência</span>
+                <h2 className="text-2xl md:text-3xl font-bold text-white mt-2">Bruno Bertolini</h2>
               </div>
-              <ul class="py-3">
-                <li class="mb-3 flex gap-3 items-center text-gray-300 text-sm"><span class="text-lg">⭐</span>Engenheiro de software e empreendedor</li>
-                <li class="mb-3 flex gap-3 items-center text-gray-300 text-sm"><span class="text-lg">⭐</span>Dezenas de projetos reais no currículo</li>
-                <li class="mb-3 flex gap-3 items-center text-gray-300 text-sm"><span class="text-lg">⭐</span>Líder técnico front-end, mobile, CTO em várias empresas</li>
-                <li class="mb-3 flex gap-3 items-center text-gray-300 text-sm"><span class="text-lg">⭐</span>Recrutou, liderou, ensinou e mentorou dezenas de programadores.</li>
+              <ul className="py-3">
+                <li className="mb-3 flex gap-3 items-center text-gray-300 text-sm"><span className="text-lg">⭐</span>Engenheiro de software e empreendedor</li>
+                <li className="mb-3 flex gap-3 items-center text-gray-300 text-sm"><span className="text-lg">⭐</span>Dezenas de projetos reais no currículo</li>
+                <li className="mb-3 flex gap-3 items-center text-gray-300 text-sm"><span className="text-lg">⭐</span>Líder técnico front-end, mobile, CTO em várias empresas</li>
+                <li className="mb-3 flex gap-3 items-center text-gray-300 text-sm"><span className="text-lg">⭐</span>Recrutou, liderou, ensinou e mentorou dezenas de programadores.</li>
               </ul>
             </div>
           </div>
